22f4845b4a08fbc61f456f496b72c29a64620d36
devesh2206200 <dgaur22062004@gmail.com>
2025-02-18 23:30:24 +0530

    Update 22BCS15715_Devesh Gaur ex.4.1.cpp

--- 22BCS15715_Devesh Gaur ex.4.1.cpp
+++ 22BCS15715_Devesh Gaur ex.4.1.cpp
@@ -1,30 +1,19 @@
-#include <iostream>
-#include <vector>
-using namespace std;
-
-vector<int> beautifulArray(int n) {
-    vector<int> res = {1};
-    while (res.size() < n) {
-        vector<int> temp;
-        for (int num : res) {
-            if (2 * num - 1 <= n)
-                temp.push_back(2 * num - 1);
+class Solution {
+public:
+    vector<int> beautifulArray(int n) {
+        vector<int> res = {1};
+        while (res.size() < n) {
+            vector<int> temp;
+            for (int num : res) {
+                if (2 * num - 1 <= n)
+                    temp.push_back(2 * num - 1);
+            }
+            for (int num : res) {
+                if (2 * num <= n)
+                    temp.push_back(2 * num);
+            }
+            res = temp;
         }
-        for (int num : res) {
-            if (2 * num <= n)
-                temp.push_back(2 * num);
-        }
-        res = temp;
+        return res;
     }
-    return res;
-}
-
-int main() {
-    int n = 4;
-    vector<int> result = beautifulArray(n);
-    for (int num : result) {
-        cout << num << " ";
-    }
-    cout << endl;
-    return 0;
-}
+};
